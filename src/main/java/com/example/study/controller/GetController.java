02168b8559a645caaf8a8c3d3a80e46e01c37ae5
danwook <k.danwoo91@ibksystem.co.kr>
2020-02-23 16:57:41 +0900

    패스트캠퍼스 강의 2일차

--- src/main/java/com/example/study/controller/GetController.java
+++ src/main/java/com/example/study/controller/GetController.java
@@ -1,8 +1,8 @@
 package com.example.study.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api") //localhost:8080/api에 매핑됨
@@ -12,7 +12,28 @@ public class GetController {
     public String getRequest(){
         return "Hi getMethod";
     }
-    
+
+    @GetMapping("/getParameter") //localhost:8080/api/getParameter?id=1234password=abcd 로 매핑되게함
+    public String getParameter(@RequestParam String id, @RequestParam(name="password") String pwd){
+
+        String password = "bbbb";
+
+        System.out.println("id: "+id);
+        System.out.println("pwd: "+pwd);
+
+        return id+password;
+    }
+
+    //localhost:8080/api/multiParameter?account=abcd&email=study@gmail.com&page=10
+    @GetMapping("/getMultiParameter")
+    public String getMultiParameter(SearchParam searchParam){
+        System.out.println(searchParam.getAccount());
+        System.out.println(searchParam.getEmail());
+        System.out.println(searchParam.getPage());
+
+        return "ok";
+
+    }
 
 
 }
