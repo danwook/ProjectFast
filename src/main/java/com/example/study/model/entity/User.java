41103f5d40371ea9d4c022578fa9261333666968
danwook <k.danwoo91@ibksystem.co.kr>
2020-02-23 21:54:52 +0900

    패스트캠퍼스 강의 2일차

--- src/main/java/com/example/study/model/entity/User.java
+++ src/main/java/com/example/study/model/entity/User.java
@@ -2,20 +2,24 @@ package com.example.study.model.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class User{
-    public User() {
-    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String account;
+
+    private String email;
 
     private String phoneNumber;
 
@@ -23,7 +27,7 @@ public class User{
 
     private String createdBy;
 
-    private LocalDateTime updateAt;
+    private LocalDateTime updatedAt;
 
     private String updatedBy;
 
