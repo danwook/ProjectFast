3b08de94c31af8297546631afd64f78aa8bddecf
danwook <k.danwoo91@ibksystem.co.kr>
2020-02-29 16:31:00 +0900

    패스트캠퍼스 강의 7일차

---
+++ src/main/java/com/example/study/controller/api/ItemApiController.java
@@ -0,0 +1,43 @@
+package com.example.study.controller.api;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.ItemApiRequest;
+import com.example.study.model.network.response.ItemApiResponse;
+import com.example.study.service.ItemApiLogicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/item")
+public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+
+    @Autowired
+    private ItemApiLogicService itemApiLogicService;
+
+    @Override
+    @PostMapping("") // api/item 라는 주소로 매핑
+    public Header<ItemApiResponse> create(@RequestBody  Header<ItemApiRequest> request) {
+
+        return null;
+    }
+
+    @Override
+    @GetMapping("{id}")
+    public Header<ItemApiResponse> read(@PathVariable Long id) {
+        return null;
+    }
+
+    @Override
+    @PutMapping("")
+    public Header<ItemApiResponse> update(@RequestBody  Header<ItemApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    @DeleteMapping("{id}")
+    public Header delete(@PathVariable Long id) {
+        return null;
+    }
+
+}
--- src/main/java/com/example/study/service/ItemApiLogicService.java
+++ src/main/java/com/example/study/service/ItemApiLogicService.java
@@ -1,8 +1,36 @@
 package com.example.study.service;
 
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.ItemApiRequest;
+import com.example.study.model.network.response.ItemApiResponse;
+import com.example.study.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ItemApiLogicService {
+public class ItemApiLogicService implements CrudInterface<ItemApiRequest, ItemApiResponse> {
 
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Override
+    public Header<ItemApiResponse> create(Header<ItemApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header<ItemApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<ItemApiResponse> update(Header<ItemApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
 }
